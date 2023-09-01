284eca02b203f09f13f3cc3e6953d49a8cca3de1
Len1viy <vadim.nevara@gmail.com>
2023-09-01 18:07:16 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+struct Matrix {
+    int array;
+};
+
+
+int Dialog() {
+    int n;
+    int m;
+    std::cout << "Введи количеcтво столбцов в матрице: " << std::endl;
+    std::cin >> n;
+    std::cout << "Введи количеcтво строк в матрице: " << std::endl;
+    std::cin >> m;
+    std::cout << "n = " << n << "; m = " << m << std::endl;
+
+    return 0;
+}
+
+
 int main() {
-    std::cout << "HI2" << std::endl;
+    Dialog();
     return 0;
 }
